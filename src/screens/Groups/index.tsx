6049478ce13161c1006b7dc0b2cbc74b3991cf16
Pedro Santos <pedrosantos.ti.web@gmail.com>
2022-10-24 19:06:45 -0300

    add component Button & EmptyList

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 
 import { Container } from './styles';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header />
@@ -14,7 +21,26 @@ export default function Groups() {
         subtitle='Jogue com a sua turma'
       />
 
-      <GroupCard title='Galera do Ignite' />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message='Que tal cadastrar a primeira turma?'
+          />
+        )}
+      />
+
+      <Button
+        title='Criar nova turma'
+      />
+
     </Container>
   );
 }
