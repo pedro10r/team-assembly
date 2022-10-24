bc8e9b748aec5d5e912b03a12c9820efd8ce9f31
Pedro Santos <pedrosantos.ti.web@gmail.com>
2022-10-24 18:26:59 -0300

    add components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,8 +1,20 @@
+import { GroupCard } from '@components/GroupCard';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+
 import { Container } from './styles';
 
 export default function Groups() {
   return (
     <Container>
+      <Header />
+
+      <Highlight
+        title='Turmas'
+        subtitle='Jogue com a sua turma'
+      />
+
+      <GroupCard title='Galera do Ignite' />
     </Container>
   );
 }
