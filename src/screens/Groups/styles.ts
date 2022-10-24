7c768b42e43677af021612c77d11a2ffe35ea1e9
Pedro Santos <pedrosantos.ti.web@gmail.com>
2022-10-24 15:48:30 -0300

    add fonts and loading

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,12 +2,6 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #131313;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const Title = styled.Text`
-  color: #FFF;
-  font-size: 32px;
+  background-color: ${({ theme }) => theme.colors.gray_600};
+  padding: 24px;
 `;
