feded5e75da60420ba786842c6a89f2d4ee77727
pedro10r <pedrosantos.ti.web@gmail.com>
2022-10-28 15:13:17 -0300

    add navigation for back initial screen

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,10 +1,19 @@
+import { useNavigation } from "@react-navigation/native";
+
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate('players', { group: 'Rocket' });
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -24,6 +33,7 @@ export function NewGroup() {
         <Button
           title='Criar'
           style={{ marginTop: 20 }}
+          onPress={handleNew}
         />
       </Content>
     </Container>
